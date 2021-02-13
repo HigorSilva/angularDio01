587de3b768b938025ab6b779ffb9e18a81837a91
Higor Silva <higorsilva.1979@hotmail.com>
2021-02-13 14:38:36 -0300

    versao 1

---
+++ src/app/page/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
+
+import { select, Store } from '@ngrx/store';
+
+import * as fromHomeActions from './state/home.actions';
+import * as fromHomeSelectors from './state/home.selectors';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
+})
+export class HomePage implements OnInit {
+
+  searchControl!: FormControl;
+
+  text!: string;
+
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.searchControl = new FormControl('', Validators.required);
+
+    this.store.pipe(select(fromHomeSelectors.selectHomeText))
+    .subscribe(text => this.text = text)
+  }
+
+  doSearch() {
+    const text = this.searchControl.value;
+    this.store.dispatch(fromHomeActions.changeText({ text }));
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: 'df090c0b96e12458da1daf6ba2dd629a',
+};
